09192d7b070c22fd44effe6a65615419c4f939cf
elicookies <elikoleva32@gmail.com>
2023-02-01 18:38:33 +0000

    adding bugzap

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,26 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet
+{
+
+	public void settings()
+	{
+		size(1000, 1000);
+	}
+
+	public void setup() {
+		colorMode(HSB);
+		background(0);
+		
+	}
+
+	float x1, y1, x2, y2;
+	float x1dir, x2dir, y1dir, y2dir;
+	float c = 0;
+	
+	public void draw()
+	{	
+	}
+}
