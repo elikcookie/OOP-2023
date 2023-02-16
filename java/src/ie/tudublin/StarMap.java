b57eeb0a0c67be8b7d8dc86f7f30aaeb6bf90e08
elicookies <elikoleva32@gmail.com>
2023-02-16 10:17:17 +0000

    base

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,6 +1,7 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+// Elitsa Koleva Lab test 2016 practice
 
 public class StarMap extends PApplet
 {
@@ -17,29 +18,9 @@ public class StarMap extends PApplet
 		
 
 
-	}
-
-
-	public void drawGrid()
-	{
-		stroke(255);
-		float border = 50.0f;
-
-		int count = 10;
-		float gap = (width - (border * 2.0f)) / (float) count;
-		for(int i = -5 ; i <= 5 ; i ++)
-		{
-			float x = border + (gap * (i + 5));
-			line(x, border, x, height - border);
-			line(border, x, width - border, x);
-		}
-		
-	}
-		
+	}	
 	public void draw()
 	{	
-		strokeWeight(2);		
-
-		drawGrid();
+			
 	}
 }
