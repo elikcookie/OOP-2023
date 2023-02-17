c5b9a3673d9bc43abaee34cfe43a537e63c14a71
elicookies <elikoleva32@gmail.com>
2023-02-17 17:13:08 +0000

    simple grid

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -19,8 +19,26 @@ public class StarMap extends PApplet
 
 
 	}	
+
 	public void draw()
 	{	
+		strokeWeight(2);
+		drawGrid();	
+	}
+
+	public void drawGrid()
+	{
+		float border = 50.0f;
+
+		for (int i = -4 ; i <= 5 ; i++){
+			stroke(255);
+			line(border, height/2 + i * 50, width - border, height/2 + i * 50);
+			line(width/2 + i * 50, border, width/2 + i * 50, height - border);
+		}
+
 			
 	}
+
+
 }
+
