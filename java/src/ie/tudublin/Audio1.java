d54bf65bacd704ac75586d47438dcb4956ae8206
elicookies <elikoleva32@gmail.com>
2023-02-20 12:41:11 +0000

    line detects sound rainbow

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,6 +1,5 @@
 package ie.tudublin;
 
-;
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
@@ -20,10 +19,16 @@ public class Audio1 extends PApplet
 		size(500, 500);
 	}
 
+	int frameSize = 1024;
+
 	public void setup() {
 		colorMode(HSB);
 		background(0);
 
+		minim = new Minim(this);
+
+		ai = minim.getLineIn(Minim.MONO, frameSize, 44100,16); // type,buffersize, samplerate, bitdepth/4410 = input frequency, 16 bit sample
+		ab = ai.mix; //mix of left and right channels
 		
 
 		smooth();
@@ -34,6 +39,17 @@ public class Audio1 extends PApplet
 	
 	public void draw()
 	{	
-		
+		//plot the samples in the buffer
+		background(0);
+		stroke(255);
+		float halfScreen = height /2 ;
+
+		float cgap = 255/ (float)ab.size();
+
+		for(int i = 0 ; i < ab.size() ; i ++)
+		{
+			stroke(cgap*i,255,255);
+			line(i, halfScreen, i, halfScreen + ab.get(i)*1000);
+		}
 	}
 }
