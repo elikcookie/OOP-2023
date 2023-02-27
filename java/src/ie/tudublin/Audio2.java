2f3e0e812f2479b3f7f1f51ad98d60ce98ebaa30
elicookies <elikoleva32@gmail.com>
2023-02-27 11:32:01 +0000

    init

---
+++ java/src/ie/tudublin/Audio2.java
@@ -0,0 +1,33 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class Audio2 extends PApplet
+{
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        //fullScreen(P3D, SPAN);
+    }
+
+    public void setup()
+    {
+
+
+    }
+
+
+    public void draw()
+    {
+        
+    }        
+}
